34a87895449117c97e05f14b3dea1e914a705ef4
Pablo2113 <pagonort@gmail.com>
2025-07-02 12:39:55 +0200

    Controller Fix

--- src/main/java/aiss/bitbucketminer/controller/BitbucketController.java
+++ src/main/java/aiss/bitbucketminer/controller/BitbucketController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 import aiss.bitbucketminer.model.REPOSITORY_POJO.Commit_Repository;
 import org.springframework.web.client.RestTemplate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -23,6 +26,9 @@ import java.util.Set;
 @RestController
 @RequestMapping("/bitbucket")
 public class BitbucketController {
+
+
+    private static final Logger log = LoggerFactory.getLogger(BitbucketController.class);
 
     @Autowired
     private BitbucketService bitbucketService;
@@ -194,33 +200,7 @@ public class BitbucketController {
 
     //----------------------------------------PETICIONES POST-----------------------------------------------------------
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{workspace}/{repoSlug}/commits")
-    public ResponseEntity<String> postCommits(
-            @PathVariable String workspace,
-            @PathVariable String repoSlug,
-            @RequestParam(defaultValue = "5") Integer nCommits,
-            @RequestParam(defaultValue = "2") Integer maxPages) {
 
-        Commit_Repository repoData = bitbucketService.getProjectsFromBitbucket(workspace, repoSlug);
-
-        GitMinerProject project = projectTransformerService.transform(repoData, new ArrayList<>(), new ArrayList<>());
-
-        String projectName = project.getName();
-
-        Commit commitData = bitbucketService.getCommitsFromBitbucket(workspace, repoSlug, nCommits, maxPages);
-        //Transformamos esos datos al formato GitMiner
-        List<GitMinerCommit> gitMinerCommits = CommitTransformerService.transform(commitData);
-
-        // URL del servicio donde se guardará el commit
-        String postUrl = "http://localhost:8080/gitminer/commits";
-
-        // Realizar la petición POST
-        ResponseEntity<String> response = restTemplate.postForEntity(postUrl, gitMinerCommits, String.class);
-
-        // Devolver el estado y un mensaje de éxito
-        return ResponseEntity.status(response.getStatusCode()).body("Commit guardado con éxito en la base de datos.");
-    }
 
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -286,122 +266,6 @@ public class BitbucketController {
         // Devolver el estado y un mensaje de éxito
         return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
     }
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{workspace}/{repoSlug}/issues")
-    public ResponseEntity<String> postIssues(
-            @PathVariable String workspace,
-            @PathVariable String repoSlug,
-            @RequestParam(required = false) Integer nCommits,
-            @RequestParam(required = false) Integer maxPages) {
-
-        // Obtener datos de los issues
-        Issues issuesData = bitbucketService.getIssuesFromBitbucket(workspace, repoSlug,nCommits,maxPages);
-        List<GitMinerIssues> resultado = new ArrayList<>();
-
-        // Verificar si se obtuvieron issues
-        if (issuesData != null && issuesData.getValues() != null) {
-            // Obtener los comentarios y usuarios de todos los issues
-            List<GitMinerComment> allComments = new ArrayList<>();
-            List<GitMinerUser> allUsers = new ArrayList<>();
-
-            for (Value value : issuesData.getValues()) {
-                Users userData = bitbucketService.getUserFromBitbucket(value.getReporter().getAccountId());
-                GitMinerUser gitMinerUser = UserTransformerService.transform(userData);
-                allUsers.add(gitMinerUser);
-                Comments commentsData = bitbucketService.getCommentsFromBitbucket(workspace, repoSlug, value.getId());
-                if (commentsData != null && commentsData.getValues() != null) {
-                    for (aiss.bitbucketminer.model.COMMENT_POJO.Value commentValue : commentsData.getValues()) {
-                        Users commentUserData = bitbucketService.getUserFromBitbucket(commentValue.getUser().getAccountId());
-                        GitMinerUser commentUser = UserTransformerService.transform(commentUserData);
-                        GitMinerComment comment = CommentsTransformService.transform(commentValue, value.getId(), commentUser);
-                        if (comment != null) {
-                            allComments.add(comment);
-                        }
-                    }
-                }
-            }
-
-            // Transformar todos los issues
-            resultado = IssuesTransformerService.transform(issuesData, allComments, allUsers);
-        }
-
-        // URL del servicio donde se guardará el issue
-        String postUrl = "http://localhost:8080/gitminer/issues";
-
-        // Realizar la petición POST
-        ResponseEntity<String> response = restTemplate.postForEntity(postUrl, resultado, String.class);
-
-        // Devolver el estado y un mensaje de éxito
-        return ResponseEntity.status(response.getStatusCode()).body("Issue guardado con éxito en la base de datos.");
-    }
-
-
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{workspace}/{repoSlug}/issues/{issuesId}/comments")
-    public ResponseEntity<String> postComments(
-            @PathVariable String workspace,
-            @PathVariable String repoSlug,
-            @PathVariable Integer issuesId,
-            @RequestParam(required = false) Integer nCommits,
-            @RequestParam(required = false) Integer maxPages) {
-
-        Comments commentsData = bitbucketService.getCommentsFromBitbucket(workspace, repoSlug, issuesId);
-        List<GitMinerComment> result = new ArrayList<>();
-
-        for (aiss.bitbucketminer.model.COMMENT_POJO.Value value : commentsData.getValues()) {
-            Users userData = bitbucketService.getUserFromBitbucket(value.getUser().getAccountId());
-            GitMinerUser gitMinerUser = UserTransformerService.transform(userData);
-
-            GitMinerComment comment = CommentsTransformService.transform(value, issuesId, gitMinerUser);
-            if (comment != null) {
-                result.add(comment);
-            }
-        }
-
-        // URL del servicio donde se guardará el comentario
-        String postUrl = "http://localhost:8080/gitminer/comments";
-
-        // Realizar la petición POST
-        ResponseEntity<String> response = restTemplate.postForEntity(postUrl, result, String.class);
-
-        // Devolver el estado y un mensaje de éxito
-        return ResponseEntity.status(response.getStatusCode()).body("Comentario guardado con éxito en la base de datos.");
-    }
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/users/{userId}")
-    public ResponseEntity<String> postUser(
-            @PathVariable String userId,
-            @RequestParam(required = false) Integer nCommits,
-            @RequestParam(required = false) Integer maxPages) {
-        // Obtener datos del usuario
-        Users userdata = bitbucketService.getUserFromBitbucket(userId);
-
-        // Transformar datos del usuario
-        GitMinerUser gitMinerUser = UserTransformerService.transform(userdata);
-
-        // URL del servicio donde se guardará el usuario
-        String postUrl = "http://localhost:8080/gitminer/users";
-
-        // Realizar la petición POST
-        ResponseEntity<String> response = restTemplate.postForEntity(postUrl, gitMinerUser, String.class);
-
-        // Devolver el estado y un mensaje de éxito
-        return ResponseEntity.status(response.getStatusCode()).body("Usuario guardado con éxito en la base de datos.");
-    }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
